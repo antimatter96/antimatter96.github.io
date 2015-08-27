20903ff9d9623d3fefea0ae37364a8e9ab40f796
antimatter96 <antimatter96@users.noreply.github.com>
2015-08-27 22:56:16 +0530

    Update main_js.js

--- main_js.js
+++ main_js.js
@@ -1,1 +1,26 @@
-// Do something
+var smoothscroller = (function(){
+	function scroller(e){
+		e.preventDefault();
+		target = (document.getElementById(this.getAttribute('href').split('#')[1])).offsetTop;
+		var myinterval;
+		function scroll() {
+			if (window.scrollY >=target) {
+				clearInterval(myinterval);
+			} 
+			else {
+				window.scrollTo(window.scrollX, window.scrollY + 10);
+			}
+        }
+		myinterval = setInterval(scroll,1);
+	}
+	return{
+		init:function(){
+			var temp = document.getElementsByTagName('a');
+			for( var i =0 ; i<temp.length ; i++ ){
+				if(temp[i].getAttribute('href')[0]==="#"){
+					temp[i].addEventListener('click',scroller);
+				}
+			}
+		}
+	}
+})();
