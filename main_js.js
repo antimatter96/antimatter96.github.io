6d0166d98426109620aad77501921cdf032b903c
Arpit Jain <antimatter96@users.noreply.github.com>
2015-09-14 01:54:55 +0530

    Update main_js.js

--- main_js.js
+++ main_js.js
@@ -18,9 +18,36 @@ var smoothscroller = (function(){
 			var temp = document.getElementsByTagName('a');
 			for( var i =0 ; i<temp.length ; i++ ){
 				if(temp[i].getAttribute('href')[0]==="#"){
+					if(temp[i].getAttribute('href')=="#nav"||temp[i].getAttribute('href')=="#"){
+						continue;
+					}
 					temp[i].addEventListener('click',scroller);
 				}
 			}
 		}
 	}
 })();
+
+var menutoggle = (function(){
+	var menu;
+	var cross;
+	function func(){
+		cross.addEventListener('click',function(){
+			menu.style.zIndex="2";
+			cross.style.zIndex="1";
+		});
+		menu.addEventListener('click',function(){
+			cross.style.zIndex="2";
+			menu.style.zIndex="1";
+		});
+	}
+	return{
+		init:function(){
+			menu=document.getElementById('menu');	
+			cross=document.getElementById('cross');
+			func();
+		}
+	}
+})();
+
+
