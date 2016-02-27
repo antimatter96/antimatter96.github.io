464c6e8890e2aaba71ece90e4dada0ecb28123a3
Arpit Jain <antimatter96@users.noreply.github.com>
2016-02-27 22:37:40 +0530

    Ver 2.0

--- main_js.js
+++ main_js.js
@@ -1,68 +1,46 @@
-var smoothscroller = (function(){
-	function scroller(e){
+var displaceHeader = (function(){
+	var target;	
+	var limit;
+	var flag = false;
+	function popp(){
+		if(window.scrollY >= limit && !flag){
+			target.classList.add('displaced');
+			bo = flag;
+		}
+		else if(window.scrollY <= limit){
+			bo = false;
+			target.classList.remove('displaced');
+		}
+	}
+	return{
+		init:function(item){
+			target = document.getElementById(item);
+			limit = target.offsetHeight;
+			window.addEventListener('scroll',popp);
+		}
+	};
+})();
+
+var correctPadding = (function(){
+	var target;
+	var nuisance;
+	var possibletargets;	
+	function func(e){
 		e.preventDefault();
-		var correction = document.getElementById("body_header").offsetHeight;
-		target = (document.getElementById(this.getAttribute('href').split('#')[1])).offsetTop;
-		var myinterval;
-		if(window.scrollY + correction <= target){
-			function scroll1() {
-				if (window.scrollY + correction>=target) {
-					clearInterval(myinterval);
-				} 
-				else {
-					window.scrollTo(window.scrollX, window.scrollY + 5);
-				}
-			}
-			myinterval = setInterval(scroll1,0.05);
-		}
-		else{
-			function scroll() {
-				if (window.scrollY + correction < target) {
-					clearInterval(myinterval);
-				} 
-				else {
-					window.scrollTo(window.scrollX, window.scrollY - 5);
-				}
-			}
-			myinterval = setInterval(scroll,0.05);
-		}
-		
+		target.checked = false;
+		var whereTo = document.getElementById(this.attributes.href.value.split('#')[1]);
+		window.scroll(0,whereTo.offsetTop - nuisance);
 	}
 	return{
-		init:function(){
-			var temp = document.getElementsByTagName('a');
-			for( var i =0 ; i<temp.length ; i++ ){
-				if(temp[i].getAttribute('href')[0]==="#"){
-					if(temp[i].getAttribute('href')=="#nav"||temp[i].getAttribute('href')=="#"){
-						continue;
-					}
-					temp[i].addEventListener('click',scroller);
+		init:function(item,item2){
+			target = document.getElementById(item);
+			nuisance = document.getElementById(item2).offsetHeight;
+			possibletargets = document.getElementsByTagName('a');
+			for(var i=0;i<possibletargets.length;i++){
+				if(possibletargets[i].attributes.href.value[0]=='#'){
+					possibletargets[i].addEventListener('click',func);
 				}
-			}
+			}			
 		}
-	}
+	};
 })();
-
-var menutoggle = (function(){
-	var menu;
-	var cross;
-	function func(){
-		cross.addEventListener('click',function(){
-			menu.style.zIndex="2";
-			cross.style.zIndex="1";
-		});
-		menu.addEventListener('click',function(){
-			cross.style.zIndex="2";
-			menu.style.zIndex="1";
-		});
-	}
-	return{
-		init:function(){
-			menu=document.getElementById('menu');	
-			cross=document.getElementById('cross');
-			func();
-		}
-	}
-})();
-
-
