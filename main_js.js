11d210af3032fb37591dea51670218079f0962c5
antimatter96 <me.arpitjain09@gmail.com>
2023-10-17 02:35:57 +0800

    New Build Oct 2023

--- main_js.js
+++ main_js.js
@@ -1,43 +1,43 @@
-var displaceHeader = (function () {
-	var target;
-	var limit;
-	var flag = false;
-	function popp() {
-		if (window.scrollY >= limit && !flag) {
-			target.classList.add("displaced");
-		} else if (window.scrollY <= limit) {
-			target.classList.remove("displaced");
-		}
-	}
-	return {
-		init: function (item) {
-			target = document.getElementById(item);
-			limit = target.offsetHeight;
-			window.addEventListener("scroll", popp);
-		}
-	};
+const displaceHeader = (function () {
+  let target;
+  let limit;
+  let flag = false;
+  function popp() {
+    if (window.scrollY >= limit && !flag) {
+      target.classList.add("displaced");
+    } else if (window.scrollY <= limit) {
+      target.classList.remove("displaced");
+    }
+  }
+  return {
+    init: function (item) {
+      target = document.getElementById(item);
+      limit = target.offsetHeight;
+      window.addEventListener("scroll", popp);
+    }
+  };
 })();
 
-var correctPadding = (function () {
-	var target;
-	var nuisance;
-	var possibletargets;
-	function func(e) {
-		e.preventDefault();
-		target.checked = false;
-		var whereTo = document.getElementById(this.attributes.href.value.split("#")[1]);
-		window.scroll(0, whereTo.offsetTop - nuisance);
-	}
-	return {
-		init: function (item, item2) {
-			target = document.getElementById(item);
-			nuisance = document.getElementById(item2).offsetHeight;
-			possibletargets = document.getElementsByTagName("a");
-			for (var i = 0; i < possibletargets.length; i++) {
-				if (possibletargets[i].attributes.href.value[0] == "#") {
-					possibletargets[i].addEventListener("click", func);
-				}
-			}
-		}
-	};
+const correctPadding = (function () {
+  let target;
+  let nuisance;
+  let possibletargets;
+  function func(e) {
+    e.preventDefault();
+    target.checked = false;
+    const whereTo = document.getElementById(this.attributes.href.value.split("#")[1]);
+    window.scroll(0, whereTo.offsetTop - nuisance);
+  }
+  return {
+    init: function (item, item2) {
+      target = document.getElementById(item);
+      nuisance = document.getElementById(item2).offsetHeight;
+      possibletargets = document.getElementsByTagName("a");
+      for (let i = 0; i < possibletargets.length; i++) {
+        if (possibletargets[i].attributes.href.value[0] == "#") {
+          possibletargets[i].addEventListener("click", func);
+        }
+      }
+    }
+  };
 })();
